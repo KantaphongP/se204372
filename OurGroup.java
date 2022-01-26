d340f176e51ee5edaac59ec657d6eec4c788a929
KantaphongP <kantaphong.p@ku.th>
2022-01-26 16:09:04 +0700

    Update OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -7,9 +7,7 @@ public class OurGroup {
     {
         groupMembers = new Vector<String>();
         groupMembers.add("Kantaphong Pititnorasate"); //หัวหน้ากลุ่ม
-        groupMembers.add("Timakarn Akepatchimsiri");
-        groupMembers.add("Thidarat Piajan");
-        groupMembers.add("Bussakorn Palatorn");
+       
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
