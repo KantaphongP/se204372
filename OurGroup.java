903691589d22aabbeb8f136eb47ed0eb1d424c89
TimakarnA <timakarn.a@ku.th>
2022-01-26 16:12:50 +0700

    My first group changes to OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -7,6 +7,7 @@ public class OurGroup {
     {
         groupMembers = new Vector<String>();
         groupMembers.add("Kantaphong Pititnorasate"); //หัวหน้ากลุ่ม
+        groupMembers.add("Timakarn Akepatchimsiri");
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
