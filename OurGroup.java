d931d4c7b5e9891a0e95fdebde5105adb2a1dbc2
thidaratPi <thidarat.pi@ku.th>
2022-01-26 16:18:46 +0700

    Update OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -8,6 +8,7 @@ public class OurGroup {
         groupMembers = new Vector<String>();
         groupMembers.add("Kantaphong Pititnorasate"); //หัวหน้ากลุ่ม
         groupMembers.add("Timakarn Akepatchimsiri");
+        groupMembers.add("Thidarat Piajan");
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
