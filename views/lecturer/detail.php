487927d149c2500f4a72aca68a25356c9ee776a8
ThidaratPi <91516629+ThidaratPi@users.noreply.github.com>
2022-03-16 01:17:42 +0700

    e

--- views/lecturer/detail.php
+++ views/lecturer/detail.php
@@ -1,9 +1,5 @@
 <?php 
-    echo "สถานะการยื่นคำร้องขอฝึกงาน   "
-    echo "รหัสนิสิต : $student_sp->id_s <br>";
-    echo "ชื่อ : $student_sp->name_s<br>";
-    echo "เบอร์โทรศัพท์ : $student_sp->phone_s<br>";
-    echo "อีเมล : $student_sp->email<br>";
-    echo "ตำแหน่งที่ไปฝึก : $inf_pet->position_s<br>";
+    echo "สถานะการยื่นคำร้องขอฝึกงาน  $petition->status_approve <br> ";
+   echo "ข้อมูลนักศึกษา <br> ชื่อ : $petition->name_s รหัสนิสิต : $petition->id_s"
 
 ?>
