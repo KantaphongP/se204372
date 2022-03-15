9a3aa597dcf377bf5bce623e6534934175fb389d
bussakornPal <bussakorn.pal@ku.th>
2022-03-15 23:44:41 +0700

    see status

--- controllers/status_petition_controller.php
+++ controllers/status_petition_controller.php
@@ -1,8 +1,8 @@
 <?php class StatusPetitionController{
    
    public function statusPetition(){
-    //$id_s = $_GET['id_s'];
-    $id_s = 'b6220503555';
+    $id_s = $_GET['id_s'];
+    //$id_s = 'b6220503555';
     $student_sp = Student::get($id_s);
     $inf_pet = Petition::get($id_s);
     $inf_com = Company::get($id_s);
--- views/student/welcome.php
+++ views/student/welcome.php
@@ -1,1 +1,2 @@
 <br>ใบยื่นคำขอฝึกงาน <a href=?controller=login&action=petition&id_s=<?php echo $student->id_s; ?>> click</a><br>
+<br> <a href=?controller=status_petition&action=statusPetition&id_s> see status</a><br>
