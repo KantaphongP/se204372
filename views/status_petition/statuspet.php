2b8ef06c96f5db4dd56f44c23551f774f07c6ff7
bussakornPal <bussakorn.pal@ku.th>
2022-03-16 01:20:32 +0700

    testttt

--- views/status_petition/statuspet.php
+++ views/status_petition/statuspet.php
@@ -3,6 +3,6 @@
     echo "ชื่อ : $student_sp->name_s<br>";
     echo "เบอร์โทรศัพท์ : $student_sp->phone_s<br>";
     echo "อีเมล : $student_sp->email<br>";
-    echo "ตำแหน่งที่ไปฝึก : $inf_pet->position_s<br>";
+    //echo "ตำแหน่งที่ไปฝึก : $inf_pet->position_s<br>";
 
 ?>
