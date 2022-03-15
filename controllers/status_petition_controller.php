c4465a0270ada44b4a6c30a99a03c9c5577ff5e9
bussakornPal <bussakorn.pal@ku.th>
2022-03-16 01:45:13 +0700

    yess

--- controllers/status_petition_controller.php
+++ controllers/status_petition_controller.php
@@ -3,9 +3,9 @@
    public function statusPetition(){
     $id_s = $_GET['id_s'];
     //$id_s = 'b6220503555';
-    echo $id_s;
+    //echo $id_s;
     $student_sp = Student::get($id_s);
-    //$inf_pet = Petition::get($id_s);
+    $inf_pet = Petition::getInf($id_s);
     //$inf_com = Company::get($id_s);
     require_once('views/status_petition/statuspet.php');
    }
