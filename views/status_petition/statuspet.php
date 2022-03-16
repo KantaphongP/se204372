7987ccf42931c7f67cab50c03e5496a47b2f4d0d
bussakornPal <bussakorn.pal@ku.th>
2022-03-16 22:30:54 +0700

    up

--- views/status_petition/statuspet.php
+++ views/status_petition/statuspet.php
@@ -1,9 +1,19 @@
 <?php 
+    echo "สถานะการยื่นคำร้องขอฝึกงาน : $inf_pet->status_approve <br>";
     echo "รหัสนิสิต : $student_sp->id_s <br>";
     echo "ชื่อ : $student_sp->name_s<br>";
     echo "เบอร์โทรศัพท์ : $student_sp->phone_s<br>";
     echo "อีเมล : $student_sp->email<br>";
     echo "ตำแหน่งที่ไปฝึก : $inf_pet->position_s<br>";
-    echo "ตำแหน่งที่ไปฝึก : $inf_pet->name_c<br>";
+    echo "ชื่อผู้ที่จะให้ภาควิชาฯออกหนังสือ : $inf_pet->name_getbook<br>";
+    echo "ตำแหน่งของผู้ที่จะให้ภาควิชาฯออกหนังสือ : $inf_pet->position_g<br>";
+    echo "ระยะการฝึกงาน : $inf_pet->date_start ถึง $inf_pet->date_end <br>";
+    echo "ชื่อสถานประกอบการ : $inf_pet->name_c<br>";
+    echo "สถานที่ตั้ง : $inf_pet->address_c<br>";
+    echo "จำนวนค่าตอบแทน : $inf_pet->income<br>";
+    echo "ชื่อผู้ประสานงาน : $inf_pet->name_hr<br>";
+    echo "เบอร์โทรศัพท์ : $inf_pet->phone_hr<br>";
+    echo "ที่พัก : $inf_pet->apartment<br>";
+
 
 ?>
