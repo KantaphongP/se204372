73a5117bf93642616f30c875b488c187a2a0b07b
bussakornPal <bussakorn.pal@ku.th>
2022-03-16 22:45:27 +0700

    Update statuspet.php

--- views/status_petition/statuspet.php
+++ views/status_petition/statuspet.php
@@ -1,19 +1,21 @@
 <?php 
-    echo "สถานะการยื่นคำร้องขอฝึกงาน : $inf_pet->status_approve <br>";
-    echo "รหัสนิสิต : $student_sp->id_s <br>";
-    echo "ชื่อ : $student_sp->name_s $student_sp->lastname_s<br>";
-    echo "เบอร์โทรศัพท์ : $student_sp->phone_s<br>";
-    echo "อีเมล : $student_sp->email<br>";
-    echo "ตำแหน่งที่ไปฝึก : $inf_pet->position_s<br>";
-    echo "ชื่อผู้ที่จะให้ภาควิชาฯออกหนังสือ : $inf_pet->name_getbook<br>";
-    echo "ตำแหน่งของผู้ที่จะให้ภาควิชาฯออกหนังสือ : $inf_pet->position_g<br>";
-    echo "ระยะการฝึกงาน : $inf_pet->date_start ถึง $inf_pet->date_end <br>";
-    echo "ชื่อสถานประกอบการ : $inf_pet->name_c<br>";
-    echo "สถานที่ตั้ง : $inf_pet->address_c<br>";
-    echo "จำนวนค่าตอบแทน : $inf_pet->income บาท/วัน<br>";
-    echo "ชื่อผู้ประสานงาน : $inf_pet->name_hr<br>";
-    echo "เบอร์โทรศัพท์ : $inf_pet->phone_hr<br>";
-    echo "ที่พัก : $inf_pet->apartment<br>";
+    echo "สถานะการยื่นคำร้องขอฝึกงาน : $inf_pet->status_approve <br>
+    <h3>ข้อมูลนักศึกษา</h3>
+    ชื่อ : $student_sp->name_s $student_sp->lastname_s รหัสนิสิต : $student_sp->id_s <br>
+    เบอร์โทรศัพท์ : $student_sp->phone_s<br>
+    อีเมล : $student_sp->email<br><br>
+    <h3>ข้อมูลการฝึกงาน</h3>
+    ตำแหน่งที่ไปฝึก : $inf_pet->position_s<br>
+    ชื่อผู้ที่จะให้ภาควิชาฯออกหนังสือ : $inf_pet->name_getbook<br>
+    ตำแหน่งของผู้ที่จะให้ภาควิชาฯออกหนังสือ : $inf_pet->position_g<br>
+    ระยะการฝึกงาน : $inf_pet->date_start ถึง $inf_pet->date_end <br>
+    <h3>ข้อมูลสถานประกอบการ</h3>
+    ชื่อสถานประกอบการ : $inf_pet->name_c<br>
+    สถานที่ตั้ง : $inf_pet->address_c<br>
+    จำนวนค่าตอบแทน : $inf_pet->income บาท/วัน<br>
+    ชื่อผู้ประสานงาน : $inf_pet->name_hr<br>
+    เบอร์โทรศัพท์ : $inf_pet->phone_hr<br>
+    ที่พัก : $inf_pet->apartment<br>";
 
 
 ?>
