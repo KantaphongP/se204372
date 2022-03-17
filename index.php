27436a475b7b85799666f25e9cea0b42331ef92f
KantaphongP <kantaphong.p@ku.th>
2022-03-17 17:27:22 +0700

    3

--- index.php
+++ index.php
@@ -11,13 +11,7 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
 <html>
     <head></head>
     <body>
-        <?php echo "controller= ".$controller.",action=".$action;?>
-        <br>[<a href="?controller=pages&action=home">home</a>]
-        [<a href="?controller=login&action=signin">เข้าสู่ระบบ</a>]
-        [<a href="?controller=company&action=index">Company</a>]
-        [<a href="?controller=lecturer&action=signin">เข้าสู่ระบบอาจารย์</a>]
-      <br>
-         
+       
         <?php require_once("./routes.php");?>
     </body>
 </html>
--- views/lecturer/welcome.php
+++ views/lecturer/welcome.php
@@ -1,1 +1,181 @@
-<br>คำร้องขอฝึกงานทั้งหมด <a href=?controller=lecturer&action=petition> click</a>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {
+  font-family : "FC Iconic"  ,"FC Iconic", sans-serif ;
+  margin: 0; 
+  height: 100%; 
+  overflow: hidden
+}
+
+.user {
+  overflow: hidden;
+  background-color: #385a64;
+  height :80;
+}
+.user a {
+  float: right;
+  font-size: 40px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+.user label {
+  position: fixed;
+  right : 0%;
+  font-size: 40px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+
+.navbar {
+  overflow: hidden;
+  background-color: #385a64;
+  left: 0% ;
+}
+
+.navbar a {
+  float: left;
+  font-size: 25px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown .dropbtn {
+  font-size: 25px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.navbar a:hover, .dropdown:hover .dropbtn {
+  background: #59767e;;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #385a64;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  float: none;
+  color: white;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.greet label{
+  position : absolute ; 
+  top : 33%; 
+  left : 12% ;
+  font-size: 600%; 
+}
+
+.greet a{
+  position : absolute ; 
+  top : 45%; 
+  left : 16.2% ;
+  font-size: 250%;
+}
+.greet a1{
+  position : absolute ; 
+  top : 50%; 
+  left : 15.7% ;
+  font-size: 250%;
+}
+.img{
+  position : absolute ; 
+  z-index: 1;
+  top : 2%;
+  left : 55% ;
+}
+
+.footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 30%;
+    left: 0px;
+    background: #2F4F58;
+    z-index: 0;
+}
+
+.footer label{
+  position : absolute ; 
+  top : 8%; 
+  left : 2.5% ;
+  font-size: 250%;
+  color : white ;
+}
+
+</style>
+</head>
+<body>
+
+<div class="user">
+<label> <?php echo  "$lecturer->name_l  $lecturer->lastname_l";?> </label>
+<br><br><a href="?controller=login&action=signin">logout</a>
+</div>
+
+<div class="navbar">
+  <a href="#home">หน้าแรก</a>
+  <a href="?controller=company&action=indexs">ค้นหาสถานประกอบการ</a>
+  <div class="dropdown">
+    <button class="dropbtn">การฝึกงาน 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+    <a href=?controller=lecturer&action=petition> คำร้องขอฝึกงานทั้งหมด</a>
+    <a>ตรวจสอบคำร้อง</a>
+    <a>ตรวจสอบประวัติ </a>
+    </div>
+  </div> 
+</div>
+<div class ="greet">
+  <label>Welcome <?php echo  "$lecturer->name_l!";?></label>
+  <br><a> ระบบจัดการการฝึกงานแบบออนไลน์</a>
+  <br><a1>สำหรับภาควิชาวิศวกรรมคอมพิวเตอร์</a1>
+</div>
+<div class="img">
+<img src="Welcome-pana.png" alt="Welcome" width="900px" height="900px">
+</div>
+<div class="footer">
+  <label>ข่าวสารการฝึกงาน</label>
+</div>
+
+
+
+</body>
+</html>
