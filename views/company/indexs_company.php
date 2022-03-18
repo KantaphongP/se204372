66403da9d9e10e3e50e0aa6fcd84623ec9d4f2b9
ThidaratPi <91516629+ThidaratPi@users.noreply.github.com>
2022-03-18 22:53:16 +0700

    u

--- views/company/indexs_company.php
+++ views/company/indexs_company.php
@@ -152,7 +152,7 @@ table{
 
 <div class="navbar">
 <a href=?controller=login&action=home&id_s=<?php echo $student->id_s;?>>หน้าแรก</a>
-  <a href="?controller=company&action=indexs">ค้นหาสถานประกอบการ</a>
+<a href=?controller=company&action=indexs&id_s=<?php echo $student->id_s;?>>ค้นหาสถานประกอบการ</a>
   <div class="dropdown">
     <button class="dropbtn">การฝึกงาน 
       <i class="fa fa-caret-down"></i>
@@ -171,6 +171,7 @@ table{
 <form method="get" action="">
         <input type="text" name="key">
         <input type="hidden" name="controller" value="company">
+        <input type="hidden" name="id_s" value="<?php echo $student->id_s; ?>"/>
         <button type="submit" name="action" value="searchs">
 search</button>
 </form>
