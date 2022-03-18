14d5c7bfe07101258c52eb803ff88342a34c1366
KantaphongP <kantaphong.p@ku.th>
2022-03-18 21:39:26 +0700

    2

--- views/lecturer/welcome.php
+++ views/lecturer/welcome.php
@@ -14,15 +14,18 @@ body {
 .user {
   overflow: hidden;
   background-color: #385a64;
-  height :80;
+  height :90;
+  z-index:0;
 }
 .user a {
-  float: right;
+  position: absolute;
+  right : 0%;
   font-size: 40px;
   color: white;
   text-align: center;
   padding: 14px 16px;
   text-decoration: none;
+  z-index:2;
 }
 .user label {
   position: fixed;
@@ -181,201 +184,3 @@ body {
 </html>
 
 
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-body {
-  font-family : "FC Iconic"  ,"FC Iconic", sans-serif ;
-  margin: 0; 
-  height: 100%; 
-  overflow: hidden
-}
-
-.user {
-  overflow: hidden;
-  background-color: #385a64;
-  height :90;
-  z-index:0;
-}
-.user a {
-  position: absolute;
-  right : 0%;
-  font-size: 40px;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  z-index:2;
-}
-.user label {
-  position: fixed;
-  right : 0%;
-  font-size: 40px;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-
-.navbar {
-  overflow: hidden;
-  background-color: #385a64;
-  width: 100% ; 
-  
-}
-
-.navbar a {
-  float: left;
-  font-size: 25px;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-.dropdown {
-  float: left;
-  overflow: hidden;
-}
-
-.dropdown .dropbtn {
-  font-size: 25px;  
-  border: none;
-  outline: none;
-  color: white;
-  padding: 14px 16px;
-  background-color: inherit;
-  font-family: inherit;
-  margin: 0;
-}
-
-.navbar a:hover, .dropdown:hover .dropbtn {
-  background: #59767e;;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #385a64;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-.dropdown-content a {
-  float: none;
-  color: white;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  text-align: left;
-}
-
-.dropdown-content a:hover {
-  background-color: #ddd;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-.greet {
-  position : absolute ;
-  width : 100% ;
-  height : 100% ; 
-  top : 30%; 
-  left : 12% ;
-}
-.greet label{
-  position : absolute ; 
-  top : 0%; 
-  left : 0.8% ;
-  
-  font-size: 600%; 
-}
-
-.greet a{
-  position : absolute ; 
-  top : 10%; 
-  left : 4% ;
-  float : center ; 
-  font-size: 250%;
-}
-.greet a1{
-  position : absolute ; 
-  top : 14%; 
-  left : 3.5% ;
-  float : center ; 
-  font-size: 250%;
-}
-.img{
-  position : absolute ; 
-  z-index: 1;
-  top : 0%;
-  left : 50% ; 
-}
-
-
-.footer {
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    height: 30%;
-    left: 0px;
-    background: #2F4F58;
-    z-index: 0;
-}
-
-.footer label{
-  position : absolute ; 
-  top : 8%; 
-  left : 2.5% ;
-  font-size: 250%;
-  color : white ;
-}
-
-</style>
-</head>
-<body>
-
-<div class="user">
-<label> <?php echo  "$lecturer->name_l  $lecturer->lastname_l";?> </label>
-<br><br><a href="?controller=login&action=signin">logout</a>
-</div>
-
-<div class="navbar">
-  <a href="#home">หน้าแรก</a>
-  <a href="?controller=company&action=indexs">ค้นหาสถานประกอบการ</a>
-  <div class="dropdown">
-    <button class="dropbtn">การฝึกงาน 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-    <a href=?controller=login&action=petition&id_s=<?php echo $student->id_s;?>>ใบยื่นคำขอฝึกงาน </a>
-    <a href=?controller=status_petition&action=statusPetition&id_s=<?php echo $student->id_s;?>>ตรวจสอบสถานะ</a>
-
-    <a>ประวัติการฝึกงาน</a>
-    <a>ตรวจสอบคำร้อง</a>
-    <a>ตรวจสอบประวัติ </a>
-    </div>
-  </div> 
-</div>
-<div class ="greet">
-  <label>Welcome <?php echo  "$lecturer->name_l!";?></label>
-  <br><a> ระบบจัดการการฝึกงานแบบออนไลน์</a>
-  <br><a1>สำหรับภาควิชาวิศวกรรมคอมพิวเตอร์</a1>
-</div>
-<div class="img">
-<img src="Welcome-pana.png" alt="Welcome" width="100%" height="100%">
-</div>
-<div class="footer">
-  <label>ข่าวสารการฝึกงาน</label>
-</div>
-
-
-
-</body>
-</html>
