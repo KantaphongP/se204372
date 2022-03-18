ac07206006f9a73467d4392bacca29e629e79ad6
TimakarnA <timakarn.a@ku.th>
2022-03-19 02:12:54 +0700

    Update new_company.php

--- views/company/new_company.php
+++ views/company/new_company.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai&display=swap" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 body {
-  font-family : "FC Iconic"  ,"FC Iconic", sans-serif ;
+  font-family: 'Noto Sans Thai', sans-serif;
   margin: 0; 
   height: 100%; 
   overflow: hidden
@@ -130,7 +133,7 @@ th {
 body {
   margin: 1.5em;
   position : center;
-  font-family : "FC Iconic"  ,"FC Iconic", sans-serif ;
+  font-family: 'Noto Sans Thai', sans-serif;
   margin: 0; 
   height: 100%; 
   overflow: hidden
